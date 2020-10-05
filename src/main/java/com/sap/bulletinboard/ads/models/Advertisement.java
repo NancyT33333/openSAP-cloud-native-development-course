1d6cc559f76fa66f25b3600c5ede5516a49aabc1
Anna Tuzova <nancyt33333@gmail.com>
2020-10-05 17:12:15 +0300

    JPA configs added

--- src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
+++ src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
@@ -1,8 +1,19 @@
 package com.sap.bulletinboard.ads.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.hibernate.validator.constraints.NotBlank;
 
+
+
+@Entity 
 public class Advertisement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
     @NotBlank
     public String title; 
     public Advertisement (String title) {
